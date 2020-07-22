ce976c5838393d589eee7671a7bccb9a87ad4532
Keith Willis <williskeith04@gmail.com>
2020-07-21 21:02:15 -0500

    cleaned up code

--- twilio.js
+++ twilio.js
@@ -6,7 +6,6 @@ const express = require("express");
 const app = express();
 const alert = require("alert-node");
 const mustacheExpress = require("mustache-express");
-require("dotenv").config();
 
 //middleware
 app.use(express.urlencoded());
