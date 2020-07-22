41c844b6daba17c1fc11d4dcb5fae5215ca1422b
Keith Willis <williskeith04@gmail.com>
2020-07-21 20:57:31 -0500

    first-commit

---
+++ twilio.js
@@ -0,0 +1,47 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_ACCOUNT_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+const fetch = require("node-fetch");
+const express = require("express");
+const app = express();
+const alert = require("alert-node");
+const mustacheExpress = require("mustache-express");
+require("dotenv").config();
+console.log(accountSid);
+console.log(authToken);
+//middleware
+app.use(express.urlencoded());
+
+app.engine("mustache", mustacheExpress());
+app.set("views", "./views");
+app.set("view engine", "mustache");
+
+app.use("/css-1", express.static("css"));
+
+console.log();
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/send-fact", (req, res) => {
+  fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+    .then((r) => r.json())
+    .then((response) => {
+      let fact = response.text;
+      let number = req.body.number;
+      client.messages
+        .create({
+          to: `+12819048366`,
+          from: "+12512500835",
+          body: `This fact is brought to you by the Random Useless Facts API:
+          ${fact}`,
+        })
+        .then((message) => console.log(message))
+        .then(alert("Message was sent"));
+      res.redirect("/");
+    });
+});
+
+app.listen(4000, () => {
+  console.log("Server is running...");
+});
